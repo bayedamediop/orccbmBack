e7b0ae8235ff86833d773accf7ae5817073006cc
bayedamediop <bayedame1823@gmail.com>
2021-06-28 12:55:47 +0000

    liste l'ensembele des vehicule et vehicule par id

--- src/Entity/ModeleVehicule.php
+++ src/Entity/ModeleVehicule.php
@@ -2,10 +2,11 @@
 
 namespace App\Entity;
 
+use Doctrine\ORM\Mapping as ORM;
+use Doctrine\Common\Collections\Collection;
 use App\Repository\ModeleVehiculeRepository;
 use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
-use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=ModeleVehiculeRepository::class)
@@ -21,11 +22,13 @@ class ModeleVehicule
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups ({"vehicule:read"})
      */
     private $libelle;
 
     /**
      * @ORM\ManyToOne(targetEntity=Marques::class, inversedBy="modeleVehicules")
+     * @Groups ({"vehicule:read"})
      */
     private $marque;
 
--- src/Entity/Vehicules.php
+++ src/Entity/Vehicules.php
@@ -7,6 +7,7 @@ use App\Entity\ModeleVehicule;
 use Doctrine\ORM\Mapping as ORM;
 use App\Repository\VehiculesRepository;
 use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ORM\Entity(repositoryClass=VehiculesRepository::class)
@@ -15,7 +16,20 @@ use ApiPlatform\Core\Annotation\ApiResource;
  *          "add_vehicule"={
  *              "route_name"="addVehicule",
  *          },
+ *  "get_vehicule"={
+ *                   "method"="GET",
+ *                    "path" = "/admin/vehicule",
+ *                     "normalization_context"={"groups"={"vehicule:read"}},
  *          },
+ *          },
+ * itemOperations={
+ * "get_vehicule"={
+ *         "method"="GET",
+ *                    "path" = "/admin/vehicule/{id}",
+ *                     "normalization_context"={"groups"={"vehicule:read"}},
+ *              
+ *          }, 
+ *          }, 
  *        
  *     )
  */
@@ -30,36 +44,43 @@ class Vehicules
 
     /**
      * @ORM\ManyToOne(targetEntity=Clients::class, inversedBy="vehicules")
+     *  @Groups ({"vehicule:read"})
      */
     private $client;
 
     /**
      * @ORM\ManyToOne(targetEntity=ModeleVehicule::class, inversedBy="vehicules")
+     * @Groups ({"vehicule:read"})
      */
     private $modele;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups ({"vehicule:read"})
      */
     private $immatriculation;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups ({"vehicule:read"})
      */
     private $chassis;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups ({"vehicule:read"})
      */
     private $numeroMoteur;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups ({"vehicule:read"})
      */
     private $numeroBC;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Groups ({"vehicule:read"})
      */
     private $dmc;
 
